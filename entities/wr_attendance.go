a84d283fe7b29c875ebcd93222bffe164c17ce63
bingtian.yu <bingtian.yu@vhall.com>
2020-11-26 17:28:31 +0800

    增加used字段

--- entities/wr_attendance.go
+++ entities/wr_attendance.go
@@ -5,6 +5,7 @@ type WrAttendance struct {
 	UserId   int    `json:"user_id" xorm:"not null index INT(11)"`
 	Overtime string `json:"overtime" xorm:"not null comment('加班时间') DATE"`
 	Hours    int    `json:"hours" xorm:"not null default 0 comment('加班时长h') TINYINT(2)"`
+	Used     int    `json:"used"`
 	Status   int    `json:"status" xorm:"not null default 1 comment('状态 1未使用； 0已用完；2 部分使用') TINYINT(1)"`
 	Created  string `json:"created" xorm:"created"`
 	Updated  string `json:"updated" xorm:"updated"`
